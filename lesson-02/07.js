ba27fb09335f9f9081405206dc755fb01c096d3e
Yulia Starchenko <imay@amake.ru>
2023-07-25 13:51:00 +0300

    task 2-07

---
+++ lesson-02/07.js
@@ -0,0 +1,13 @@
+let passportMarried = {
+  name: "Petr",
+  surname: "Petrov",
+  address: {
+    country: "USA",
+    city: "Bobryisk",
+  },
+};
+
+const passportMarriedCopy = {...passportMarried, married: true};
+
+console.log(passportMarried);
+console.log(passportMarriedCopy);
