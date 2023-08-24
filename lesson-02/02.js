60d75b6931db7d91ccc9cb83c40c2081d79e15e7
Yulia Starchenko <imay@amake.ru>
2023-08-24 20:16:26 +0300

    03-01

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -1,5 +1,4 @@
 function factorial(n) {
-    debugger;
     if (n === 0 || n === 1) {
         return 1;
     } else {
--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+  return colors.filter(c => c === "черный" || c === "красный" || c === "желтый").join('-');
+}
+
+console.log(createColorString());
